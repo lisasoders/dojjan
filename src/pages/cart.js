369cf3a34c850a88b083bb492e8ba514ed6e6060
lisasoders <91470070+lisasoders@users.noreply.github.com>
2023-01-09 16:42:54 +0100

    total price in cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,9 +1,16 @@
 import React from "react";
 import dummyImg from "../image/dummyImg.png"
+import { useState, useEffect } from "react";
 import { FaRegTrashAlt } from 'react-icons/fa';
 
 
 function Cart( {cartItems, setCartItems} ) {
+
+    const [totalSum, setTotalSum] = useState([]);
+
+    var totalPrice = 0;
+
+    
 
     const deletedProduct = (cartItem) => {
         let deletedProduct = Object.values(cartItems).filter(i => i.id !== cartItem.id)
@@ -15,13 +22,15 @@ function Cart( {cartItems, setCartItems} ) {
             {cartItems.length === 0 ? <div className="empty-cart">Din varukorg är tom </div> :  <div className="product-cart">
                 <h1 className="cart-title">Din varukorg</h1>
                 {cartItems.map(item => (
+                    totalPrice += item.price,
                 <div className="cart-item" key={item.id}>
                     <img className="cart-img" alt="dummyImg" src={dummyImg} />
                     <p>{item.title}</p>
-                    <p>{item.price}</p>
+                    <p>{item.price} kr</p>
                     <div onClick={() => deletedProduct(item)}><FaRegTrashAlt /></div>
                 </div>
             ))}</div>}
+            <div>Totalsumma: {totalPrice}</div>
         </div>
     )
 }
