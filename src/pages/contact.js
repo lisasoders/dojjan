19344bd407c3b57f8ea14d711c1c3ffc909e439d
lisasoders <91470070+lisasoders@users.noreply.github.com>
2023-01-10 14:21:35 +0100

    styling contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { BsTelephone } from 'react-icons/bs';
+import { TfiEmail } from 'react-icons/tfi';
+
+
 
 function Contact() {
     return(
-        <div>
-            <h1>hej</h1>
+        <div className="contact-wrapper">
+            <h3 className="contact-title">KONTAKT</h3>
+            <div className="telephone-wrapper">
+                <div className="contact-telephone">
+                        <BsTelephone />
+                        <p>TELEFON</p>
+                </div>
+                <div className="contact-number">
+                        <p className="contact-text">020 120 3132</p>
+                        <p className="contact-text">Från måndag till fredag: 9:30 - 17:00</p>
+                </div>
+           </div>
+           <div className="mail-wrapper">
+                <div className="contact-mail">
+                        <TfiEmail />
+                        <p>MAIL</p>
+                </div>
+                <div className="contact-mail-info">
+                        <p className="contact-text">info@dojjan.com</p>
+                        <p className="contact-text">Från måndag till söndag: 9:00 - 15:00</p>
+                </div>
+           </div>
         </div>
     )
 }
